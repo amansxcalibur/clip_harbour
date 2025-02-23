177146edc402c1a583846094c53b043a4dcb622a
angrezichatterbox <gouthammohanraj@gmail.com>
2025-02-23 19:33:45 +0530

    [CHORE]: Remove unnecessary logging and install

--- src/components/search/search_bar.jsx
+++ src/components/search/search_bar.jsx
@@ -6,10 +6,13 @@ import { useVideo } from "../../providers/video_context";
 export default function SearchBar() {
     const [searchValue, setSearchValue] = useState("");
     const navigate = useNavigate();
-    const { setSelectedVideo } = useVideo();
+    const { setSelectedVideo , setSearchResults } = useVideo();
 
     async function Search() {
+        setSearchResults(null)
+        setSearchResults([{ title: "Loading...", uploader: "", duration: "" }]);
         if (searchValue.includes("http")) {
+            
             setSelectedVideo(null)
             invoke("get_url_details", { url: searchValue }).then((videoDetails) => setSelectedVideo(videoDetails));
             navigate("/val");
@@ -20,7 +23,7 @@ export default function SearchBar() {
 
     return (
         <div className="">
-            <input id="search" onKeyDown={(e) => { if (e.key == "Enter") Search() }} onChange={(e) => setSearchValue(e.target.value)} type="text" className="text-black text-base font-bold min-h-[3.5vw] min-w-[25vw] font-light text-[1vw] border border-[#bebebe] border-solid px-[1vw]" placeholder="Search or Enter URL" />
+            <input id="search" onKeyDown={(e) => {if (e.key == "Enter") Search()}} onChange={(e) => setSearchValue(e.target.value)} type="text" className="text-black text-base font-bold min-h-[3.5vw] min-w-[25vw] font-light text-[1vw] border border-[#bebebe] border-solid px-[1vw]" placeholder="Search or Enter URL" />
             <button className="min-w-10" onClick={Search}>Search</button>
         </div>
     )
