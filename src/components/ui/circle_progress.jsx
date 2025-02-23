a8558a3b012e22faac89e8af6ed33479619756a0
amansxcalibur <notifyamanvs@gmail.com>
2025-02-23 19:19:43 +0530

    [FIX]: Circular progress bar updates with dowload state percent

--- src/components/ui/circle_progress.jsx
+++ src/components/ui/circle_progress.jsx
@@ -1,4 +1,9 @@
-export default function CircularProgressBar({percentage}){
+import { useEffect } from "react"
+
+export default function CircularProgressBar({download}){
+    useEffect(()=>{
+        console.log(download,"hererere")
+    })
     return(
         <div className="relative size-12">
       <svg className="size-full -rotate-90" viewBox="0 0 36 36" xmlns="http://www.w3.org/2000/svg">
@@ -13,14 +18,14 @@ export default function CircularProgressBar({percentage}){
           className="stroke-current text-black"
           strokeWidth="2"
           strokeDasharray="100"
-          strokeDashoffset={100 - percentage}
+          strokeDashoffset={100-parseInt(download.percentage, 10) || 0}
           strokeLinecap="round"
         ></circle>
       </svg>
       {/* Percentage Text */}
       {/* <div className="absolute top-1/2 start-1/2 transform -translate-y-1/2 -translate-x-1/2">
         <span className="text-center text-[0.5rem] font-bold text-blue-600">
-          {percentage}%
+          {parseInt(download.percentage, 10) || 0}%
         </span>
       </div> */}
     </div>
